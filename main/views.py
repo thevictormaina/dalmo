720ca9bed3907a2e584cae46eb135e2991670ace
Victor K. Maina <vk13runic@gmail.com>
2021-10-21 17:15:57 +0300

    completed design for homepage

--- main/views.py
+++ main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    ctx = { "title": "Know Yourself" }
+    ctx = { 
+        "title": "Know Yourself",
+        "daily_rating": 3, # Change to entry object
+        "tidiness_rating": 2, # Change as well
+        }
     return render(request, 'main/index.html', ctx)
